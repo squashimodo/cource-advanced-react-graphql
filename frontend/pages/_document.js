b8b64e1c146c3edb5766d1c7619790a8343c88aa
Bentlov Hans (SEIT) <hans.bentlov@hm.com>
2018-10-26 16:28:32 +0200

    Remove log

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -3,7 +3,6 @@ import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage, ...props }) {
-    console.log(renderPage);
     const sheet = new ServerStyleSheet();
     const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
     const styleTags = sheet.getStyleElement();
