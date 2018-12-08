3d2ac31f5e4fb465013296b34539e32525f96d69
Hans Bentlöv <hanserikb@gmail.com>
2018-12-08 16:18:46 +0100

    Try using apollos graphql mutation HoC

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Mutation } from 'react-apollo';
+import { graphql } from 'react-apollo';
 import Form from './styles/Form';
-import formatCurrency from '../lib/formatMoney';
 import gql from 'graphql-tag';
 import ErrorMessage from '../components/ErrorMessage';
 import Router from 'next/router';
@@ -30,8 +29,6 @@ const CREATE_ITEM_MUTATION = gql`
   }
 `;
 
-export { CREATE_ITEM_MUTATION };
-
 class CreateItem extends Component {
   state = {
     title: '',
@@ -49,56 +46,102 @@ class CreateItem extends Component {
     });
   }
 
+  uploadFile = async (e) => {
+    const [file] = e.target.files;
+    const data = new FormData();
+    data.append('file', file);
+    data.append('upload_preset', 'Sellstuff');
+
+    const res = await fetch('https://api.cloudinary.com/v1_1/hbentlov/image/upload', {
+      method: 'POST',
+      body: data
+    });
+
+    const jsonData = await res.json();
+    this.setState({
+      image: jsonData.secure_url,
+      largeImage: jsonData.eager[0].secure_url
+    })
+  }
+
   render() {
+    const { mutate: createItem } = this.props;
+    const { loading, error } = this.state;
     return (
-      <Mutation
-        mutation={CREATE_ITEM_MUTATION}
-        variables={this.state}>
-        {(createItem, { loading, error, called, data }) => (
-          <Form onSubmit={async (e) => {
-            // Prevent the form to post
-            e.preventDefault();
+      <Form onSubmit={async (e) => {
+        // Prevent the form to post
+        e.preventDefault();
 
-            // Call the mutation
-            const res = await createItem();
+        this.setState({
+          loading: true
+        })
 
-            // Go to item page
-            Router.push({
-              pathname: '/item',
-              query: {
-                id: res.data.createItem.id
-              }
-            })
-          }}>
-          <ErrorMessage error={error}/>
-            <fieldset disabled={loading} aria-busy={loading}>
-              <Input
-                onChange={this.changeValue}
-                title="Title"
-                placeholder="Title"
-                value={this.state.title}
-                propName="title" />
-              <Input
-                type="number"
-                title="Price"
-                onChange={this.changeValue}
-                value={this.state.price}
-                propName="price" />
-              <Input
-                type="textarea"
-                onChange={this.changeValue}
-                title="Description"
-                placeholder="Description"
-                value={this.state.description}
-                propName="description" />
-            </fieldset>
-            <button type="submit">Submit</button>
-          </Form>
-        )}
-      </Mutation>
+        // Call the mutation
+        try {
+          const { data: { createItem: { id } } } = await createItem({
+            variables: this.state,
+            // optimisticResponse: {
+            //   createItem: {
+            //     __typename: 'Item',
+            //     id: -1,
+            //     ...this.state
+            //   },
+            // },
+          });
+
+          this.setState({
+            loading: false,
+            error: null
+          })
+
+          // Go to item page
+        Router.push({
+          pathname: '/',
+          // query: {
+          //   id: id
+          // }
+        })
+        } catch(e) {
+          this.setState({
+            loading: false,
+            error: e
+          })
+        }
+      }}>
+      <ErrorMessage error={error}/>
+        <fieldset loading={loading} aria-loading={loading}>
+          <Input
+            onChange={this.changeValue}
+            title="Title"
+            placeholder="Title"
+            value={this.state.title}
+            propName="title" />
+          <Input
+            type="number"
+            title="Price"
+            onChange={this.changeValue}
+            value={this.state.price}
+            propName="price" />
+          <Input
+            type="textarea"
+            onChange={this.changeValue}
+            title="Description"
+            placeholder="Description"
+            value={this.state.description}
+            propName="description" />
+            <Input
+            type="file"
+            onChange={this.uploadFile}
+            title="Upload file"
+            placeholder="Upload file"
+            propName="image" />
+        </fieldset>
+        <button type="submit">Submit</button>
+      </Form>
     );
   }
 }
+
 
 
 const Input = ({ title, type = 'text', propName, placeholder = '', value, onChange, ...props }) => {
@@ -126,4 +169,6 @@ Input.propTypes = {
   onChange: PropTypes.func
 }
 
-export default CreateItem;
+//export default CreateItem;
+export default graphql(CREATE_ITEM_MUTATION)(CreateItem)
+export { CREATE_ITEM_MUTATION };
