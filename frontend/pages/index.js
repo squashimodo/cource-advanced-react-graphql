d25e4c8f5a81c204e6b23921343e897ec7a43594
Hans Bentlöv <hanserikb@gmail.com>
2019-01-03 18:21:51 +0100

    Add some lorem ipsum content on the start page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Items from '../components/Items';
 
-const HomePage = props => (
+const HomePage = ({ query }) => (
   <div>
     <h1>Webshop</h1>
-    <Items page={parseFloat(props.query.page) || 1} />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eros felis,
+      interdum non maximus eu, vehicula eget orci. Mauris et erat libero. Sed
+      metus justo, commodo vitae turpis convallis, luctus convallis mi. Morbi
+      commodo rutrum luctus. Interdum et malesuada fames ac ante ipsum primis in
+      faucibus. Praesent a nulla sagittis turpis congue semper. Lorem ipsum
+      dolor sit amet, consectetur adipiscing elit. Cras nec iaculis dolor.
+    </p>
+
+    <Items page={parseFloat(query.page) || 1} />
   </div>
 );
 
